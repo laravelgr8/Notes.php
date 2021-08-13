6683aeed7dcc66414218bbdd15e65e9056e80b4e
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2021-08-13 14:54:50 +0530

    Update time.php

--- time.php
+++ time.php
@@ -1,2 +1,4 @@
 <?php echo date("d-m-Y", strtotime($atten->date)); ?>
 <?php  echo date('h:i a', strtotime($atten->time));  ?>
+date('Y-m-d\TH:i', strtotime($exampleDate))
+<input type="datetime-local name="date" value="date('Y-m-d\TH:i', strtotime($exampleDate))">
