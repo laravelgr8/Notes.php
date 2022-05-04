37115206f2368d7525f5c3d957b9b9c8e9ae8455
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-05-04 14:29:28 +0530

    Update time.php

--- time.php
+++ time.php
@@ -2,3 +2,12 @@
 <?php  echo date('h:i a', strtotime($atten->time));  ?>
 date('Y-m-d\TH:i', strtotime($exampleDate))
 <input type="datetime-local name="date" value="date('Y-m-d\TH:i', strtotime($exampleDate))">
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+How to get next 30 days:- 
+$today = date('Y-m-d'); //only date month year                                                                                           
+$today = date('Y-m-d h:i:s', time());  //with time
+$document_date=substr($today,0,10);  
+$checkdate=date('Y-m-d',strtotime('+30 days',strtotime($document_date)));
